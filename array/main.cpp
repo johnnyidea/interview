ca3b0122f8d058cb579d4d8eb90a6558fa3659be
Rene <khatymovrenat@gmail.com>
2022-03-16 02:36:56 +0400

    feature. Remove duplucates impl

--- array/main.cpp
+++ array/main.cpp
@@ -1,10 +1,13 @@
-#include "rem_dup.h"
-#include "tmp.h"
 #include <iostream>
+
+#include "rem_dup.h"
 
 using namespace std;
 int main()
 {
-    cout << "Hello!" << endl;
+
+    RemDup remDup;
+    remDup.run();
+
 	return 0;
 }
--- array/rem_dup/rem_dup.h
+++ array/rem_dup/rem_dup.h
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 //=======================================================================================
 /*! \class RemDup
@@ -37,7 +38,7 @@ public:
 
 private:
 
-    //! List of private variables.
+    int removeDuplicates(std::vector<int>& nums);
 
 };
 //=======================================================================================
